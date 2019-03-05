d9dc1ad83d783826330a01ff982f01df747001e1
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2019-03-05 11:21:03 +0100

    formating code (wt17)

--- src/test/java/edu/iis/mto/lab_0/TailsTest.java
+++ src/test/java/edu/iis/mto/lab_0/TailsTest.java
@@ -1,12 +1,12 @@
 package edu.iis.mto.lab_0;
 
-import static org.junit.Assert.assertThat;
-
 import java.util.List;
 
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
 
 public class TailsTest {
 

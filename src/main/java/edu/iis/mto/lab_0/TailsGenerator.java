7b9655efe9b1b1d7c1aeb66d32b9e24553fd42c4
struszczak.kamila <209472@edu.p.lodz.pl>
2019-03-05 18:12:00 +0100

    poprawienie funkcji tails(String value), aby pozostałe testy poprawnie się wykonywały

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -7,7 +7,18 @@ public class TailsGenerator {
 
     public List<String> tails(String value) {
 
-        ArrayList<String> substrings = new ArrayList();
+        ArrayList<String> substrings = new ArrayList<>();
+
+        if (value == null) {
+            return substrings;
+        }
+
+        for (int i = 0; i < value.length(); i++) {
+            substrings.add(value.substring(i));
+        }
+
+        substrings.add("");
+
         return substrings;
 
     }
